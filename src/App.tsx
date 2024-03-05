2f2873d624d85dbbd1d2589054d9b2759a9679c3
Clayton Zickerman <cczickerman@gmail.com>
2024-03-05 02:06:24 -0500

    change background color

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const { state } = useContext(AppContext)!;
 
   return (
-    <div className="h-screen bg-gray-950 flex flex-col justify-center items-center">
+    <div className="h-screen bg-black flex flex-col justify-center items-center">
       <div className="flex flex-col items-center mb-4">
         <h1 className="text-5xl text-white">Guitar Trainer</h1>
         <h2 className="text-xl text-white">Click the note {notes[state.correctNote]}</h2>
